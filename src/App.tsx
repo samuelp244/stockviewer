6e3129698a410e4eb98fb9fe8fbf10fc1b3a8090
samuelp244 <samuelp244@gmail.com>
2022-10-08 00:22:52 +0530

    implemented list sort and search feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router,Routes,Route, Navigate, Link} from "react-router-dom"
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom"
 import InstrumentsPage from './pages/InstrumentsPage';
 import Quotes from './pages/Quotes';
 function App() {
--- src/pages/InstrumentsPage.tsx
+++ src/pages/InstrumentsPage.tsx
@@ -1,27 +1,40 @@
-import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
+
+interface dataType{
+  name:string ,
+  sector:string,
+  symbol:string,
+  validTill:string,
+}
 
 const InstrumentsPage = () => {
-  const [data,setData] = useState<Array<Array<string>>>()
+
+  const [data,setData] = useState<Array<dataType>>()
+  const [searchTerm,setSearchTerm] = useState("")
+
   const navigate = useNavigate()
-  
+
   useEffect(()=>{
     axios.get('https://prototype.sbulltech.com/api/v2/instruments').then(res=>{
       const arr:string[] = res.data.split("\n")
       const finalArray = arr.map(obj=>obj.split(','))
-      setData(finalArray)
+      const temp =  finalArray.map(obj=>({symbol:obj[0],name:obj[1],sector:obj[2],validTill:obj[3]})).filter(obj=>obj.name!=='Name')
+      setData(temp.sort((a,b)=>{
+        if(a.symbol>b.symbol) return 1;
+        else if(a.symbol<b.symbol) return -1;
+        return 0
+      }))
     })
   },[])
   
-  const RowClick = (id:string)=>{
-    navigate(`/quotes/${id}`)
-  }
+  const RowClick = (id:string)=> navigate(`/quotes/${id}`)
+
 
   return (
-    
-    <div className='w-[90%] flex justify-center'>
-
+    <div className='w-[90%] flex flex-col justify-center'>
+      <input className='' placeholder='search' onChange={(e)=>setSearchTerm(e.target.value)}/>
       <table className="table border shadow-lg">
         <thead >
           <tr className=' shadow-md'>
@@ -33,23 +46,30 @@ const InstrumentsPage = () => {
         </thead>
         <tbody>
           {
-            data?.slice(1)?.map(i=>{
-              
+            data?.filter(val=>{
+              if(searchTerm===""){
+                return val
+              }else if (val.symbol?.toLowerCase().includes(searchTerm.toLowerCase())||
+                        val.name?.toLowerCase().includes(searchTerm.toLowerCase())||
+                        val.sector?.toLowerCase().includes(searchTerm.toLowerCase())||
+                        val.validTill?.toLowerCase().includes(searchTerm.toLowerCase())){
+                return val
+              }
+              return null
+            }).map(i=>{
               return(
-                <tr  key={i[0]} onClick={()=>RowClick(i[0])} className='hover:bg-slate-100 text-sm'>
-                  <th scope="row" >{i[0]}</th>
-                  <td>{i[1]}</td>
-                  <td>{i[2]}</td>
-                  <td>{i[3]}</td>
+                <tr  key={i.symbol} onClick={()=>RowClick(i.symbol)} className='hover:bg-slate-100 text-sm'>
+                  <th scope="row" >{i.symbol}</th>
+                  <td>{i.name}</td>
+                  <td>{i.sector}</td>
+                  <td>{i.validTill}</td>
                 </tr>
               )
             })
           }
         </tbody>
       </table>
-
     </div>
-
   )
 }
 
